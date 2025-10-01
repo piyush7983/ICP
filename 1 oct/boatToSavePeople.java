fbfc195aff88665d1531a285a80bcb774acdda33
piyush7983 <154682036+piyush7983@users.noreply.github.com>
2025-10-01 23:05:18 +0530

    1 oct

---
+++ 1 oct/boatToSavePeople.java
@@ -0,0 +1,25 @@
+package 1 oct;
+
+public class boatToSavePeople {
+    
+}
+import java.util.Arrays;
+
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int i = 0, j = people.length - 1;
+        int boats = 0;
+
+        while (i <= j) {
+            if (people[i] + people[j] <= limit) {
+                i++;
+                j--;
+            } else {
+                j--;
+            }
+            boats++;
+        }
+        return boats;
+    }
+}
